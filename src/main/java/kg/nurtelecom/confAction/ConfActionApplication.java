240cd159c0a444755949767ca9b5b244e4097bb8
esen <esen.puma200314@mail.ru>
2024-01-11 16:45:15 +0600

    First commit

---
+++ src/main/java/kg/nurtelecom/confAction/ConfActionApplication.java
@@ -0,0 +1,13 @@
+package kg.nurtelecom.confAction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfActionApplication.class, args);
+	}
+
+}
